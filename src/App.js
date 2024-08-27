6ba2faa5be807aa01dd9c8ccf5f9219ce3b5c9c5
Phạm Hoàng Minh Anh <anhphmse183496@fpt.edu.vn>
2024-08-27 21:39:18 +0700

    two way binding

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
 import "./App.css";
 import { useState } from "react";
 
-function App() {
-  const [info, setInfo] = useState({
-    name: "Soybean",
-    age: 18,
-    address: "Beijing",
-  });
+const courses = [
+  { id: 1, name: "Tiktok 101" },
+  { id: 2, name: "Tiktok 102" },
+  { id: 3, name: "Tiktok 103" },
+];
 
-  const handleUpdate = () => {
-    setInfo({
-      ...info,
-      bio: "I Love Pink",
+function App() {
+  const [courseId, setCourseId] = useState([]);
+
+  const handleSubmit = () => {
+    console.log(courseId);
+  };
+  const handleCheck = (id) => {
+    setCourseId(prev => {
+      if (prev.includes(id)) {
+        return prev.filter(item => item !== id)
+      } else {
+        return [...prev, id]
+      }
     });
   };
   return (
     <div className="App">
-      <h1>{JSON.stringify(info)}</h1>
-      <button onClick={handleUpdate}>Update</button>
+      {courses.map((course) => (
+        <div key={course.id}>
+          {course.name}
+          <input
+            type="checkbox"
+            checked={courseId.includes(course.id)}
+            onChange={() => handleCheck(course.id)}
+          ></input>
+        </div>
+      ))}
+      <button onClick={handleSubmit}>Register</button>
     </div>
   );
 }
