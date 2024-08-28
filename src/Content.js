d6e58ee41ae81c34612d5bb87c5857d44eb644c2
Phạm Hoàng Minh Anh <anhphmse183496@fpt.edu.vn>
2024-08-28 13:20:44 +0700

    setInterval

--- src/Content.js
+++ src/Content.js
@@ -1,21 +1,19 @@
 import { useEffect, useState } from "react";
 
 function Content() {
-  const [width, setWidth] = useState(window.innerWidth);
-  const [height, setHeight] = useState(window.innerHeight);
+  const [countDown, setCountDown] = useState(180);
 
   useEffect(() => {
-    const handleResize = () => {
-      setWidth(window.innerWidth);
-      setHeight(window.innerHeight);
-    };
-    window.addEventListener("resize", handleResize);
-    return () => window.removeEventListener("resize", handleResize);
+    const interval = setInterval(() => {
+      setCountDown(prev => prev - 1);
+    }, 1000);
+
+    return () => clearInterval(interval);
   }, []);
 
   return (
     <div>
-      <h1>{width + "x" + height}</h1>
+      <h1>{countDown}</h1>
     </div>
   );
 }
