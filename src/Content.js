d196f69026f9531048acdb3996b3c074a6051d4a
Phạm Hoàng Minh Anh <anhphmse183496@fpt.edu.vn>
2024-08-28 12:54:03 +0700

    useEfftect with DOM events

--- src/Content.js
+++ src/Content.js
@@ -1,41 +1,21 @@
 import { useEffect, useState } from "react";
 
-const tabs = ["posts", "comments", "albums"];
-
 function Content() {
-  const [title, setTitle] = useState("");
-  const [posts, setPosts] = useState([]);
-  const [type, setType] = useState("posts");
+  const [width, setWidth] = useState(window.innerWidth);
+  const [height, setHeight] = useState(window.innerHeight);
 
   useEffect(() => {
-    fetch(`https://jsonplaceholder.typicode.com/${type}`)
-      .then((res) => res.json())
-      .then((posts) => setPosts(posts));
-  }, [type]);
+    const handleResize = () => {
+      setWidth(window.innerWidth);
+      setHeight(window.innerHeight);
+    };
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
 
   return (
     <div>
-      {tabs.map((tab) => (
-        <button
-          key={tab}
-          style={
-            type === tab ? { color: "red", backgroundColor: "yellow" } : {}
-          }
-          onClick={() => setType(tab)}
-        >
-          {tab}
-        </button>
-      ))}
-      <input
-        type="text"
-        value={title}
-        onChange={(e) => setTitle(e.target.value)}
-      />
-      <ul>
-        {posts.map((post) => (
-          <li key={post.id}>{post.title || post.name}</li>
-        ))}
-      </ul>
+      <h1>{width + "x" + height}</h1>
     </div>
   );
 }
