cd9edc579480861370e00226e5a8acc5fa377403
Phạm Hoàng Minh Anh <anhphmse183496@fpt.edu.vn>
2024-08-28 12:40:06 +0700

    useEffect

--- src/Content.js
+++ src/Content.js
@@ -1,5 +1,43 @@
+import { useEffect, useState } from "react";
+
+const tabs = ["posts", "comments", "albums"];
+
 function Content() {
-  return <h1>Hello World!</h1>;
+  const [title, setTitle] = useState("");
+  const [posts, setPosts] = useState([]);
+  const [type, setType] = useState("posts");
+
+  useEffect(() => {
+    fetch(`https://jsonplaceholder.typicode.com/${type}`)
+      .then((res) => res.json())
+      .then((posts) => setPosts(posts));
+  }, [type]);
+
+  return (
+    <div>
+      {tabs.map((tab) => (
+        <button
+          key={tab}
+          style={
+            type === tab ? { color: "red", backgroundColor: "yellow" } : {}
+          }
+          onClick={() => setType(tab)}
+        >
+          {tab}
+        </button>
+      ))}
+      <input
+        type="text"
+        value={title}
+        onChange={(e) => setTitle(e.target.value)}
+      />
+      <ul>
+        {posts.map((post) => (
+          <li key={post.id}>{post.title || post.name}</li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
 export default Content;
